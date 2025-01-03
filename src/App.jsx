82f1b2d1a82b6e1229d48985ebb19c3c3fd0b1ae
Laksh Kumar <73332171+lakshkumar06@users.noreply.github.com>
2025-01-02 20:33:46 -0500

    Dependencies installed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Homepage';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        
+      
+
+        <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Homepage />} /> 
+
+
+
+      </Routes>
+    </BrowserRouter>
+  
+    </div>
+
+    </>
+  )
+}
+
+export default App
